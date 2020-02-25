fe7000782d68f9b9ff8704cc3b8a74b276d7a9a8
aluc <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2020-02-26 01:53:58 +0900

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# parse yes24
+[yes24](http://www.yes24.com) is online bookstore in Korea.
+
+## Parse by single book id
+```shell
+$ node parse.js <yes24-book-id>
+```
+
+If web url is `http://www.yes24.com/Product/Goods/87631628` yes24-book-id is `87631628`
+
+Example:
+```shell
+$ node parse.js 87631628
+parse bookId: 87631628
+{
+  "title": "공부, 이래도 안되면 포기하세요",
+  "subTitle": "무조건 합격을 부르는 최강의 멘탈 솔루션 ",
+  "mainImgUrl": "http://image.yes24.com/goods/87631628/800x0"
+}
+```
+
+## Parse by yaml file (multiple book id)
+
+```
+$ node parse.js <filename>
+```
+
+Example:
+
+yes24-book-id.yml:
+```yml
+- 87631628  # 공부, 이래도 안되면 포기하세요
+- 65282018  # 하버드 상위 1퍼센트의..
+```
+
+```shell
+$ node parse.js yes24-book-id.yml
+parse bookId: 65282018
+parse bookId: 87631628
+[
+  {
+    "title": "공부, 이래도 안되면 포기하세요",
+    "subTitle": "무조건 합격을 부르는 최강의 멘탈 솔루션 ",
+    "mainImgUrl": "http://image.yes24.com/goods/87631628/800x0"
+  },
+  {
+    "title": "하버드 상위 1퍼센트의 비밀 (리커버 에디션)",
+    "subTitle": "신호를 차단하고 깊이 몰입하라 ",
+    "mainImgUrl": "http://image.yes24.com/goods/65282018/800x0"
+  }
+]
+```
---
+++ parse.js
@@ -0,0 +1,51 @@
+const fs = require('fs')
+const cheerio = require('cheerio')
+const axios = require('axios')
+const yaml = require('js-yaml')
+const delay = require('delay')
+
+const ymlFileContent = fs.readFileSync('./yes24-book-id.yml', 'utf8')
+const json = yaml.safeLoad(ymlFileContent)
+
+const parseYes24BookInfo = async (yes24BookId) => {
+  console.log('parse bookId: ' + yes24BookId)
+  const r = await axios.request({
+    method: 'get',
+    url: `http://www.yes24.com/Product/Goods/${yes24BookId}`,
+  })
+  const $ = cheerio.load(r.data)
+
+  const title = $('#yDetailTopWrap > div.topColRgt > div.gd_infoTop > div > h2').text()
+  const subTitle = $('#yDetailTopWrap > div.topColRgt > div.gd_infoTop > div > h3').text()
+  const mainImgUrl = $('#yDetailTopWrap > div.topColLft > div.gd_imgArea > span > em > img').attr('src')
+  // TODO: 필요한 정보 더 추가하기
+
+  return {
+    title,
+    subTitle,
+    mainImgUrl,
+  }
+}
+
+const runBySingleId = async (yes24BookId) => {
+  const parsed = await parseYes24BookInfo(yes24BookId)
+  console.log(JSON.stringify(parsed,null,2))
+}
+
+const runByYml = async () => {
+  const promiseArr = json.map( async (yes24BookId,i) => {
+    // 부하를 줄이기 위해 요청당 100ms 의 딜레이를 주고 보냄.
+    await delay(i * 100);
+    return parseYes24BookInfo(yes24BookId)
+  })
+
+  const resultArr = await Promise.all(promiseArr )
+  console.log(JSON.stringify(resultArr,null,2))
+}
+
+if (require.main === module) {
+  const bookIdOrYmlFileName = process.argv[2]
+  if (/\.ya?ml$/.test(bookIdOrYmlFileName)) runByYml(bookIdOrYmlFileName)
+  else if (/^\d+?$/.test(bookIdOrYmlFileName)) runBySingleId(bookIdOrYmlFileName)
+  else throw new Error('Please check README.md')
+}
